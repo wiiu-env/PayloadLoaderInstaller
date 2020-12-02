a3ad75cc2cf96f6e30a1441e98ba62db762b7b69
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2020-12-02 15:57:13 +0100

    Reorganize menu layout

--- source/ApplicationState.h
+++ source/ApplicationState.h
@@ -5,6 +5,7 @@
 #include "common/common.h"
 #include "InstallerService.h"
 #include "Input.h"
+#include "Menu.h"
 
 class ApplicationState {
 public:
@@ -21,34 +22,44 @@ public:
         STATE_WELCOME_SCREEN,
         STATE_GET_APP_INFORMATION,
         STATE_CHECK_PATCH_POSSIBLE,
+        STATE_CHECK_COLDBOOT_STATUS,
         STATE_CHECK_REMOVAL_POSSIBLE,
-        STATE_INSTALL_MENU,
-        STATE_INSTALL_CHOOSE_COLDBOOT,
-        STATE_INSTALL_NO_COLDBOOT_ALLOWED,
+        STATE_COMPATIBILITY_RESULTS,
+        STATE_APP_INCOMPATIBLE,
+        STATE_MAIN_MENU,
         STATE_INSTALL_CONFIRM_DIALOG,
         STATE_INSTALL_STARTED,
         STATE_INSTALL_BACKUP,
         STATE_INSTALL_FST,
         STATE_INSTALL_COS,
         STATE_INSTALL_RPX,
-        STATE_INSTALL_SYSTEM_XML,
         STATE_INSTALL_SUCCESS,
         STATE_REMOVE_CONFIRM_DIALOG,
         STATE_REMOVE_STARTED,
         STATE_REMOVE_COLDBOOT,
         STATE_REMOVE_AROMA,
-        STATE_REMOVE_SUCCESS
+        STATE_REMOVE_SUCCESS,
+        STATE_BOOT_MENU,
+        STATE_BOOT_SWITCH_SYSMENU,
+        STATE_BOOT_SWITCH_AROMA,
+        STATE_BOOT_SWITCH_SUCCESS,
+        STATE_EXIT_SYSMENU,
+        STATE_EXIT_SHUTDOWN,
     };
 
     ApplicationState();
 
     void setError(eErrorState error);
 
+    void changeState(eGameState newState);
+
     void render();
 
     void update(Input *input);
 
     void checkPatchPossible();
+
+    void checkColdbootStatus();
 
     void checkRemovalPossible();
 
@@ -60,24 +71,23 @@ public:
 
     std::string ErrorDescription();
 
-    void handleError();
-
-    int selectedOption;
-
-    static void printFooter();
-
-    void proccessMenuNavigation(Input *input, int maxOptionValue);
-
-    static bool entrySelected(Input *input);
-
     bool installColdboot = false;
     InstallerService::eResults installerError = InstallerService::eResults::SUCCESS;
 
 private:
+    Menu<eGameState> menu;
+
     bool fstPatchPossible = false;
     bool cosPatchPossible = false;
-    bool systemXMLPatchPossible = false;
+
     bool removalPossible = false;
+    bool installPossible = false;
+    bool alreadyInstalled = false;
+
+    bool systemXMLPatchPossible = false;
+    bool systemXMLPatchAllowed = false;
+    bool systemXMLAlreadyPatched = false;
+    bool systemXMLRestorePossible = false;
 
     eGameState state;
     eErrorState error = ERROR_NONE;
